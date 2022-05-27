ae4b42dd026f1d29f2c3508fcc4f87d0eec3ffe6
Yosuke Kato <yaw@ginnojo.jp>
2022-05-27 22:22:01 +0900

    Correct Mongoid configs

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,15 @@ module BsPlaylistSort
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Set Mongoid to be default
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
