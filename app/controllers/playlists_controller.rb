246619effa2d107e75f06ca3ea7e4d55f5acf3d4
Yosuke Kato <yaw@ginnojo.jp>
2022-06-01 01:52:04 +0900

    Add Playlist fields and json upload pages

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,13 +1,59 @@
 class PlaylistsController < ApplicationController
   def index
+    # Clear existing records during development
+    Playlist.destroy_all
   end
 
   def show
+    # Only my playlist is available
+    if session.blank? || session[:playlist_id].blank? || (session[:playlist_id] != params[:id])
+      redirect_to playlists_path
+    end
+
+    @rec = Playlist.find(params[:id])
+    redirect_to playlists_path if @rec.blank?
   end
 
   def new
+    # Nothing to do
   end
 
   def create
+    # !!Implement safety checks
+    json = JSON.parse(params[:upload_file].read)
+
+    rec = build_playlist(json: json)
+    rec.filename = params[:upload_file].original_filename
+    if rec.save!
+      session[:playlist_id] = rec.id.to_s
+      redirect_to playlist_path(rec)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def build_playlist(json: nil)
+    rec = Playlist.new
+
+    field_list.each do |field, json_field|
+      rec[field] = json[json_field]
+      json.delete(json_field)
+    end
+
+    json.delete('songs')
+    rec.others = json
+
+    rec
+  end
+
+  def field_list
+    {
+      playlist_title: 'playlistTitle',
+      playlist_author: 'playlistAuthor',
+      playlist_description: 'playlistDescription',
+      image: 'image'
+    }
   end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,21 @@
 class Playlist
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  # validations
+  validates \
+    :playlist_title,
+    :image,
+    :filename,
+    presence: true
+
+  # playlist contents
+  field :playlist_title, type: String
+  field :playlist_author, type: String
+  field :playlist_description, type: String
+  field :image, type: String
+  field :others, type: Hash
+
+  # playlist data
+  field :filename, type: String
 end
