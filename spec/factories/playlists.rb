b9cc7bd2d8e6d5f693fa036fb45ccd84e3224245
Yosuke Kato <yaw@ginnojo.jp>
2022-06-05 23:36:35 +0900

    WIP: Add Playlist model specs

--- spec/factories/playlists.rb
+++ spec/factories/playlists.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :playlist do
-    
+    playlist_title { Faker::String.random }
+    image { 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABASURBVDhPvcohDsAwAMSw/v/TG4jxaaQzjHL+8nxgjTZZo03WaJM12mSNNlmjTdZokzXaZI02WaNN1miT9bJzXhQtrmD22l8jAAAAAElFTkSuQmCC' }
+    filename { "#{Faker::Alphanumeric.alphanumeric(number: 10)}.json" }
+
+    trait :full_fields do
+      playlist_author { Faker::String.random }
+      playlist_description { Faker::String.random }
+      others do
+        { 'syncURL' => "https://bsaber.com/PlaylistAPI/#{Faker::Alphanumeric.alphanumeric(number: 10)}.json" }
+      end
+    end
   end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'field checks' do
+    let(:rec) { build(:playlist) }
+
+    it 'is valid with required fields' do
+      expect(rec).to be_valid
+    end
+
+    it 'is invalid without required fields' do
+      rec.playlist_title = ''
+      rec.image = ''
+      rec.filename = ''
+
+      expect(rec).to_not be_valid
+      expect(rec.errors[:playlist_title]).to include("can't be blank")
+      expect(rec.errors[:image]).to include("can't be blank")
+      expect(rec.errors[:filename]).to include("can't be blank")
+    end
+
+    it 'is valid with full fields' do
+      expect(build(:playlist, :full_fields)).to be_valid
+    end
+  end
 end
