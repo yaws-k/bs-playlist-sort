3fa9c08c2d1b2a0975ecefa86f5c8d7d9213d75b
Yosuke Kato <yaw@ginnojo.jp>
2022-06-08 00:29:13 +0900

    Add Request spec

--- spec/requests/playlists_spec.rb
+++ spec/requests/playlists_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "Playlists", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Playlists', type: :request do
+  describe '#index' do
+    it 'returns http success' do
+      get '/playlists'
+      expect(response).to have_http_status(:success)
+    end
+
+    context 'with root address' do
+      it 'returns http success' do
+        get '/'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#show' do
+    let(:playlist) { create(:playlist) }
+
+    context 'without valid session' do
+      it 'returns http redirect' do
+        get "/playlists/#{playlist.id}"
+        expect(response).to redirect_to playlists_path
+      end
+    end
+  end
+
+  describe '#new' do
+    it 'returns http success' do
+      get '/playlists/new' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#create' do
+    it 'returns http redirect' do
+      post '/playlists', params: {
+        upload_file: fixture_file_upload('valid_playlist.json')
+      }
+      expect(response).to redirect_to playlist_path(Playlist.order(created_at: :desc).first)
+    end
   end
 end
